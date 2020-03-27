5a53efd24a12850000a4527769459e3f56265b5b
Anandh003 <angaravi003@gmail.com>
2020-03-27 08:53:45 +0530

    Fix: Remove all deprecated functions
    Signed-off-by: Anandh003 <angaravi003@gmail.com>

--- tf_tutorial.py
+++ tf_tutorial.py
@@ -39,8 +39,8 @@ learning_rate = 0.001
 epochs = 100
 batch_size = 32
 
-x = tf.placeholder(tf.float32, [None, 784])
-y = tf.placeholder(tf.float32, [None, 10])
+x = tf.compat.v1.placeholder(tf.float32, [None, 784])
+y = tf.compat.v1.placeholder(tf.float32, [None, 10])
 
 W1 = tf.Variable(tf.random.normal([784, 300], stddev=0.03), name='W1')
 b1 = tf.Variable(tf.random.normal([300]), name='b1')
@@ -54,26 +54,26 @@ X = tf.matmul(A1, W2)
 A2 = tf.nn.softmax(tf.add(tf.matmul(A1, W2), b2))
 A2 = tf.clip_by_value(A2, 1e-10, 0.9999999)
 
-cross_entropy = -tf.reduce_mean(tf.reduce_sum((y * tf.log(A2))+ ((1-y) * tf.log(1 - A2)), axis=1))
+cross_entropy = -tf.reduce_mean(tf.reduce_sum((y * tf.math.log(A2))+ ((1-y) * tf.math.log(1 - A2)), axis=1))
 
-optimiser = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
+optimiser = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
 
-inti_op = tf.global_variables_initializer()
-correct_prediction = tf.equal(tf.arg_max(y, 1), tf.arg_max(A2, 1))
+inti_op = tf.compat.v1.global_variables_initializer()
+correct_prediction = tf.equal(tf.math.argmax(y, 1), tf.math.argmax(A2, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
-with tf.Session() as sess:
+with tf.compat.v1.Session() as sess:
     sess.run(inti_op)
-    total_batch = int(len(mnist.train.labels) / batch_size)
+    total_batch = int(x_train.shape[0] / batch_size)
     for epoch in range(epochs):
         avg_cost = 0
         for batch in range(total_batch):
-            batch_x, batch_y = mnist.train.next_batch(batch_size=batch_size)
+            batch_x, batch_y = train_x[(batch_size * batch) : (batch_size * batch+1)], train_y[(batch_size * batch) : (batch_size * batch+1)]
             _, c = sess.run([optimiser, cross_entropy],
                             feed_dict={x: batch_x, y: batch_y})
             avg_cost += c / total_batch
         print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost))
-    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
+    print("Test Accuracy", sess.run(accuracy, feed_dict={x: test_x, y: test_y}))
 
 
